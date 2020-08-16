a789deb25819e25ba9c19a1ae65304e5dd55f811
Patryk J <jamroz.patryk@gmail.com>
2020-08-16 22:20:20 +0200

    You can add to-dos

---
+++ index.js
@@ -0,0 +1,17 @@
+const entryForm = document.getElementById("entryForm");
+const tasksSection = document.getElementById("tasks");
+const entryTextBox = document.getElementsByClassName('entry-textbox');
+
+
+function addEntryToDom(event) {
+    event.preventDefault();
+    const entryDiv = document.createElement("div");
+    entryDiv.className = 'single-task';
+    entryDiv.innerText = entryTextBox[0].value;
+    tasksSection.appendChild(entryDiv);
+    entryTextBox[0].value = ''
+    
+
+}
+
+entryForm.addEventListener('submit', addEntryToDom);
